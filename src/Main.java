65d8fddc6113694735f747ca3af725fe33a8a5fc
alba1002 <108678930+alba1002@users.noreply.github.com>
2022-07-16 18:15:05 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+
 public class Main {
+    private static Boss boss;
+
+    public Main() {
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Hero(500, "Shild", 200, "Fly");
+        new Hero(500, 100);
+        new Boss();
+        int BossHealth = 1000;
+        int BossDamage = 400;
+        String typeDefence = "Shild";
+        String superPower = "superjump";
+        System.out.println("" + BossHealth + " " + BossDamage + " " + typeDefence + superPower);
     }
 }
