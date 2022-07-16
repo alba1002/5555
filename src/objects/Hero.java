60fac381e29c782e15dedaad4705a86087bb9285
alba1002 <108678930+alba1002@users.noreply.github.com>
2022-07-16 18:14:49 +0600

    Update Hero.java

--- src/objects/Hero.java
+++ src/objects/Hero.java
@@ -1,2 +1,33 @@
-package objects;public class Hero {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Hero {
+    private int Health;
+    private int Damage;
+    private String typeDefence;
+    private String superPower;
+
+    public Hero(int Health, String typeDefence, int Damage, String superPower) {
+    }
+
+    public Hero(int TheHealth, int TheDamage) {
+    }
+
+    public int getHealth() {
+        return this.Health;
+    }
+
+    public int getDamage() {
+        return this.Damage;
+    }
+
+    public String getTypeDefence() {
+        return this.typeDefence;
+    }
+
+    public String getSuperPower() {
+        return this.superPower;
+    }
 }
