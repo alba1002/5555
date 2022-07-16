a5f37bbd8ce9676cd3e6e646a426edc4f9031156
alba1002 <108678930+alba1002@users.noreply.github.com>
2022-07-16 18:14:24 +0600

    Update Boss.java

--- src/objects/Boss.java
+++ src/objects/Boss.java
@@ -1,2 +1,37 @@
-package objects;public class Boss {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fer
+
+public class Boss {
+    private int Health;
+    private int Damage;
+    private String typeDefence;
+
+    public Boss() {
+    }
+
+    public int getHealth() {
+        return this.Health;
+    }
+
+    public int getDamage() {
+        return this.Damage;
+    }
+
+    public String getTypeDefence() {
+        return this.typeDefence;
+    }
+
+    public void setHealth(int Health) {
+        this.Health = Health;
+    }
+
+    public void setDamage(int Damage) {
+        this.Damage = Damage;
+    }
+
+    public void setTypeDefence(String typeDefence) {
+        this.typeDefence = typeDefence;
+    }
 }
+
